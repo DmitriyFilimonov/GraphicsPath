9c8593a0b90cf35a53c6c8742396f6678a58c5cb
Dmitriy <banodannon@gmail.com>
2020-12-02 20:00:37 +0300

    NO tmpBitmap

--- GraphicsPath/Form1.cs
+++ GraphicsPath/Form1.cs
@@ -21,12 +21,16 @@ namespace GraphicsPath
 
         private Rectangle _rect;
         private bool _mouseDown;
+        private Bitmap _mainBitmap;
+        private Graphics _graphics;
         
 
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
+
             
             e.Graphics.DrawPolygon(new Pen(Color.Blue, 5),  new PointF[] { _rect.Location, new PointF(_rect.Location.X + _rect.Width, _rect.Location.Y + _rect.Height) });
+            e.Graphics.DrawImage(_mainBitmap, new Point(0,0));
         }
 
         private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
@@ -55,7 +59,15 @@ namespace GraphicsPath
             _rect.Width = e.Location.X - _rect.Location.X;
             _rect.Height = e.Location.Y - _rect.Location.Y;
             pictureBox1.Invalidate(_rect);
+            _graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            _graphics.DrawPolygon(new Pen(Color.Blue, 5), new PointF[] { _rect.Location, new PointF(_rect.Location.X + _rect.Width, _rect.Location.Y + _rect.Height) });
             _mouseDown = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _mainBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            _graphics = Graphics.FromImage(_mainBitmap);
         }
     }
 }
